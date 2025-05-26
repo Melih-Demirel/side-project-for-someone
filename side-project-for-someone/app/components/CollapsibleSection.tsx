1c8979a723b8fbf51ab8c41ad0b8e3ba7957ae1a
MelihD-1953139 <melih.demirel@student.uhasselt.be>
2025-05-26 03:23:21 +0200

    m

--- side-project-for-someone/app/components/CollapsibleSection.tsx
+++ side-project-for-someone/app/components/CollapsibleSection.tsx
@@ -24,16 +24,19 @@ export default function CollapsibleSection({
     <div className="my-2">
       <button
         onClick={() => setOpen(!open)}
-        className="w-full text-left font-semibold text-black py-2 px-3 bg-gray-100 hover:bg-gray-200 rounded"
+        className={`w-full text-left font-semibold text-black py-2 px-3 hover:bg-gray-200 bg-gray-100 rounded-t-md`}
       >
         {open ? "▼ " : "▶ "} {title}
       </button>
+
       {open && typeof content === "string" ? (
-        <p className="px-3 mt-2 text-gray-800 whitespace-pre-line">{content}</p>
+        <p className="px-3 pt-2 pb-3 text-gray-800 whitespace-pre-line bg-gray-100 rounded-b-md">
+          {content}
+        </p>
       ) : (
         open &&
         isObjectContent(content) && (
-          <div className="px-3 mt-2 text-gray-800 space-y-2 whitespace-pre-line">
+          <div className="px-3 pt-2 pb-3 text-gray-800 space-y-2 whitespace-pre-line bg-gray-100 rounded-b-md">
             <p>{content.intro}</p>
             <ul className="list-disc list-inside space-y-1">
               {content.bullets.map((item, i) => (
@@ -44,6 +47,8 @@ export default function CollapsibleSection({
           </div>
         )
       )}
+
+
     </div>
   );
 }
--- side-project-for-someone/app/components/Navbar.tsx
+++ side-project-for-someone/app/components/Navbar.tsx
@@ -13,6 +13,12 @@ export default function Navbar() {
   return (
     <nav className="fixed top-0 left-0 right-0 bg-white shadow z-50">
       <div className="max-w-4xl mx-auto px-4 py-2 flex items-center justify-between">
+        {/* Logo or Brand could go here */}
+        <h3 className="text-l md:text-l font-extrabold text-gray-900 mb-0 flex-grow">
+          Alles Over Jongeren
+        </h3>
+
+
         {/* Desktop Menu */}
         <div className="hidden md:flex flex-wrap justify-center gap-3">
           {sections.map((section) => (
